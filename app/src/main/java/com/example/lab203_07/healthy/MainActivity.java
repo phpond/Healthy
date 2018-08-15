6a5ff55f9ac211ab51e5bf203cd074f4940e258b
phpond <59070182@it.kmitl.ac.th>
2018-08-15 12:14:42 +0700

    bla

---
+++ app/src/main/java/com/example/lab203_07/healthy/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab203_07.healthy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.fragment_login);
+//        setContentView(R.layout.fragment_register);
+        setContentView(R.layout.fragment_bmi);
+    }
+}
